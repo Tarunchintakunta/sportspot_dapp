9361a76357c0ee90d32bbe314a742864ab4c503c
Tarun chintakunta <tarunchintakunta@gmail.com>
2025-04-13 00:31:39 +0530

    fix: update package.json versions and scripts, modify hardhat.config.js, and enhance vercel.json configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,17 @@
-// hardhat.config.js - Copy and paste this entire file
-const { HardhatUserConfig } = require("hardhat/config");
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-const config = {
+module.exports = {
   solidity: "0.8.17",
-  defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY]
-    }
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   paths: {
-    root: "./",
-    sources: "./contracts",
     artifacts: "./frontend/src/artifacts",
-    cache: "./cache",
-    tests: "./test"
-  }
+  },
 };
-
-module.exports = config;
